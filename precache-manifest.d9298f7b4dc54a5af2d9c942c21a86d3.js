30c557a95efced9b1b494544a558a8395650ec3a
Oleksandr Bodrov <oleksandr.bodrov@bazaarvoice.com>
2019-07-19 11:00:08 +0300

    Updates

---
+++ precache-manifest.d9298f7b4dc54a5af2d9c942c21a86d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795bc0b370bc811f4a2fec957961c54a",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "1e700d549f3592de7749",
+    "url": "/toDoList/static/css/main.3f564c8d.chunk.css"
+  },
+  {
+    "revision": "a8950823c7c2b273b7e9",
+    "url": "/toDoList/static/js/2.18b99cd1.chunk.js"
+  },
+  {
+    "revision": "1e700d549f3592de7749",
+    "url": "/toDoList/static/js/main.a4875f39.chunk.js"
+  },
+  {
+    "revision": "9722a55e69eb4ad6e976",
+    "url": "/toDoList/static/js/runtime~main.930cf86c.js"
+  }
+]);
